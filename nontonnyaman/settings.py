c6895a88b8f1ad73a18c94a7125ce1219a779db2
winaldo.amadea <winaldoaa@gmail.com>
2023-09-29 20:39:33 +1000

    Change procfile,requirements.txt, and db

--- nontonnyaman/settings.py
+++ nontonnyaman/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
