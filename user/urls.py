4b12d126b0a806233ca218120af4ceb65518d7a6
raihan.fadhila01 <raihan.fadhila01@ui.ac.id>
2023-10-03 23:13:42 +1000

    Fixing urls.py

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
      path('flu-register-user/', flutter_register_user),
      path('flu-login/',flutter_user_login),
      path('user_info/', flutter_user_info, name='user_info'),
-     path('edit_user/', views.flutter_edit_user, name='edit_user'),
+     path('edit_user/', flutter_edit_user, name='edit_user'),
 ]
