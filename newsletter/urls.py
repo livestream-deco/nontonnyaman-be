e60e827359bd6f2a5938b4d8aa86650f5581e127
Bryant Tanujaya <bryanttanujaya@Bry-MacBook-Pro.local>
2023-10-04 16:44:02 +1000

    Commit newsletters feature DONE

---
+++ newsletter/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from newsletter.views import add_newsletter, view_detail_newsletter,view_all_newsletter
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+     path('add-newsletter/', add_newsletter),
+     path('view-detail-newsletter/', view_detail_newsletter),
+     path('view-all-newsletter/', view_all_newsletter),
+
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ newsletter/views.py
@@ -0,0 +1,71 @@
+import json
+from django.shortcuts import render
+from newsletter.forms import NewsletterForm
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from newsletter.models import Newsletter
+from django.http import HttpResponse, JsonResponse
+
+from django.shortcuts import get_object_or_404, render,HttpResponseRedirect
+
+from newsletter.models import Newsletter
+
+def add_newsletter(request):
+    if require_http_methods(["POST"]):
+        context = {}
+        context['form'] = NewsletterForm(request.POST, request.FILES)
+        if context['form'].is_valid():
+            context['form'].save()
+            context['form'] = NewsletterForm()
+        return render(request, 'newsletter.html', context)   
+
+# def add_newsletter(request):
+#     if request.method ==  "POST":
+#         form = NewsletterForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('add-newsletter/')
+
+@csrf_exempt
+def view_detail_newsletter(request):
+    newsletter_id = request.GET.get('input_id')
+    print(newsletter_id)
+    newsletter_id2 = request.GET.get('id')
+    print(newsletter_id2)
+    newsletter = Newsletter.objects.get(id=newsletter_id)
+    newsletter_list = []
+    print(newsletter.newsletter_picture.url)
+    newsletter_list.append({
+        'newsletter_id' : newsletter.id,
+        'newsletter_title' : newsletter.newsletter_title,
+        'newsletter_text': newsletter.newsletter_text,
+        'newsletter_picture': json.dumps(str(newsletter.newsletter_picture.url)) if newsletter.newsletter_picture else None,
+    })
+    print('masuk')
+    data = json.dumps(newsletter_list)
+    return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def view_all_newsletter(request):
+    newsletters = Newsletter.objects.all()
+        
+    return render(request, 'viewall.html', 
+        {
+		'newsletters':newsletters})
+
+def delete_newsletter(request,id):
+    context ={}
+ 
+    # fetch the object related to passed id
+    obj = get_object_or_404(Newsletter, id = id)
+ 
+ 
+    if request.method =="POST":
+        obj.delete()
+        return HttpResponseRedirect("list/")
+    
+    return render(request, "delete_view.html", context)
+
+def newsletterhtmk(request):
+    obj=Newsletter.objects.all()
+    return render(request,'list.html',{"obj":obj})
