80e1ae28571d9f9a77c2d2415748f1f5d98440c6
reynaldi0810 <reynaldi.oktavianus8@gmail.com>
2023-10-16 18:22:50 +1000

    create some backend feature

--- user/urls.py
+++ user/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
      path('register-staff/', register_staff, name='add_staff'),
      path('list_staff/', list_staff, name='list_staff'),
      path('choose_staff/', choose_staff, name='choose_staff'),
-     path('user_info/', flutter_user_info, name='user_info'),
+     path('user_info/', flutter_get_user_info, name='user_info'),
      path('edit_user/', flutter_edit_user, name='edit_user'),
      path('staff-detail/',staff_detail),
-     path('user-info/',flutter_get_user_info),
+     path('user-info-detail/',flutter_user_info),
+     path('confirm-user/',confirm_user),
+     path('info-staff/',info_staff),
+     path('end-task/', end_task)
 ]
